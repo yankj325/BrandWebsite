3d1ab1fcb400ef5924c776a418efcc3411acf185
yankj325 <yankj325@163.com>
2020-04-27 17:24:24 +0800

    brandwebsite by andy

---
+++ nisscampus/js/common.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ac5c64adbf98692cf1392bcb77662e50";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
---
+++ plumparcel/js/common.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2e9f6fa399f860351338b8ef07362582";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
